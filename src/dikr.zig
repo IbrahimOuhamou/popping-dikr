9736e7a14efea54fb79a70b252f8d4a7142306ac
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-23 08:52:50 +0000

    now uses a config slice all thanks to Allah

--- src/dikr.zig
+++ src/dikr.zig
@@ -16,7 +16,19 @@ const c = @cImport({
 const screen_h: u16 = 0;
 const screen_w: u16 = 0;
 
-var config = struct {
+const config_zon =
+    \\.{
+    \\  .font_size = 80,
+    \\
+    \\  .bg_color = .{ .r = 255, .g = 255, .b = 255, .a = 255 },
+    \\  .text_color = .{ .r = 0, .g = 0, .b = 0, .a = 255 },
+    \\
+    \\  .sleep_time_minutes = 16,
+    \\  .display_time_seconds = 5,
+    \\}
+;
+
+const Config = struct {
     font_size: u8 = 80,
 
     bg_color: c.SDL_Color = .{ .r = 255, .g = 255, .b = 255, .a = 255 },
@@ -24,11 +36,15 @@ var config = struct {
 
     sleep_time_minutes: u16 = 16,
     display_time_seconds: u16 = 5,
-}{};
+};
+
+var config: Config = undefined;
 
 const bismi_allah: [:0]const u8 = "ﻥﺎﻤﺣﺮﻟﺍ";
 
 pub fn main() !void {
+    config = try std.zon.parse.fromSlice(Config, std.heap.c_allocator, config_zon, null, .{ .ignore_unknown_fields = true });
+
     errdefer |err| if (err == error.SdlError) std.log.err("SDL error: {s}", .{c.SDL_GetError()});
 
     // For programs that provide their own entry points instead of relying on SDL's main function
